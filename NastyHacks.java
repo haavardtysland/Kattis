c5c3ef73ddf256981a2ab2df33d163bc4e696844
haavarty <haavarty@stud.ntnu.no>
2019-11-11 22:17:10 +0100

    Added new files

---
+++ NastyHacks.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class NastyHacks {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++ ) {
+            int r = sc.nextInt();
+            int e = sc.nextInt();
+            int c = sc.nextInt();
+            if ((e-c) > r) {
+                System.out.println("advertise");
+            } else if ((e-c) == r) {
+                System.out.println("does not matter");
+            } else {
+                System.out.println("do not advertise");
+            }
+        }
+    }
+}
